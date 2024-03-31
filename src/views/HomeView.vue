90b303098eeef26a9c40f7c8a5788d9881ad5ef1
landminehq <z1216198007@gmail.com>
2024-04-01 00:39:14 +0800

    update about page and other files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
+  <header>
+    <RouterLink to="/about">关于界面</RouterLink>
+  </header>
   <main>
-    <TheWelcome />
+    欢迎加入Motar我的世界服务器，这里有最好玩的冒险与地牢2整合生存！
+    QQ群：483949851，加入后即可获取客户端，还有更多的游戏攻略和交流。
+    服务器已稳定运行一年，拥有强大的硬件配置和优质的网络环境，保证你的游戏体验无与伦比。
+    RAD2是一个超大型的魔法奇幻冒险生存整合包，你可以在这里体验到无尽的探索和挑战，与你的队友一起完成超过1500个任务，探索超过100个新生物群系，穿越12个不同的维度，挑战数10种新地牢，收集各种稀有的装备和材料，打造你的专属武器和防具，成为这个世界的最强者！
+    还等什么，快来加入我们吧，一起享受这个神奇的冒险之旅吧！
   </main>
 </template>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 120px 0;
+}
+</style>
